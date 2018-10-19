42cb1276d93a042ed685f922340f3d7d7769e595
Brittany Heller <brittany.heller@live.com>
2018-10-19 17:46:13 -0600

    Add project files.

---
+++ CodeShopWarehouse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeShopWarehouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
